34f67f934344587b904b39b67da8561eac763468
thiagobordini <thiagobordini@outlook.com>
2025-05-11 13:12:59 -0300

    Meu terceiro commit no arquivo principal

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ print(f'Olá, me chamo {nome}! E tenho {idade} anos.')
 
 import pandas as pd
 
-def minha_funcao():
+def minha_funcao() -> None:
     print("Olá, Mundo!")
-
 minha_funcao()
+
+if nome == 'Thiago Bordini':
+    print('Yes name')
+else:
+    print('Not name')
+
+for i in range(1, 11):
+    print(i)
+    
