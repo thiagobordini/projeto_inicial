fc52ad38642248a0f50e711e051e58cb57d04b1c
thiagobordini <thiagobordini@outlook.com>
2025-05-11 12:03:44 -0300

    Meu segundo commit no arquivo principal

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ nome = 'Thiago Bordini'
 idade = 19
 
 print(f'Olá, me chamo {nome}! E tenho {idade} anos.')
+
+import pandas as pd
+
+def minha_funcao():
+    print("Olá, Mundo!")
+
+minha_funcao()
